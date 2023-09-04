0c27f453822e13bf75892ba84a456b6d2cfa7f15
Marie Therese Jaum <theresejaum@gmail.com>
2023-09-04 23:57:10 +0800

    Initial Change

--- App.js
+++ App.js
@@ -1,20 +1,178 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
+
+const PP = () => {
+  return(
+    <View>
+      <Image source={{ uri: 'https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/366500307_3182156585426021_3881239628149660026_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=52f669&_nc_eui2=AeHNd3dx68nTA3CfWINfBMA3hY757w2177KFjvnvDbXvsmKYy1DrF1Dnz_rDbajqk_RPAbapb4RMI4UJekaFz0sy&_nc_ohc=H2T0W7WwE-oAX9w8I9m&_nc_zt=23&_nc_ht=scontent.fceb6-1.fna&oh=00_AfBIi6zFQEPZMz9ByncL1csf5Z0BL4JkIJiYYtQVDnMG_g&oe=64FB8D4B' }} style ={styles.pp} />
+    </View>
+  ) 
+};
+const Profile = () => {
+  const fname = 'Marie Therese'
+  const lname = 'Jaum'
+  return(
+    <View>
+      <Text style ={styles.profile}>{fname} {lname}</Text>
+    </View>
+  )
+};
+const Course = props =>{
+  return(
+    <View>
+      <Text style ={styles.course}>{props.like}</Text>
+    </View>
+  )
+};
+const Subject = props => {
+  return(
+    <View>
+      <Text style={styles.sub} >{props.subcode}</Text>
+    </View>
+  )
+};
+
+const SubjectDesc = props => {
+  return(
+    <View>
+      <Text style={styles.subdesc}>{props.subdesc}</Text>
+    </View>
+  )
+};
+const Units = props => {
+  return(
+    <View>
+      <Text style={styles.units}>Units: {props.units}</Text>
+    </View>
+  )
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <View style={styles.head}>
+        <PP/>
+        <Profile/>
+        <Course like = "BS INFORMATION TECHNOLOGY"/>
+      </View>
+
+      <Text style={styles.subtitle}>My Subjects</Text>  
+      
+      <View style={styles.sublist}>
+        <Subject  subcode= 'ELEC 2'/>
+          <View>
+            <SubjectDesc subdesc= 'Web Development/Web Enterprise'/>
+            <Units units='3'/>
+          </View> 
+      </View>
+      <View style={styles.sublist}>
+        <Subject  subcode= 'ELEC 3'/>
+          <View>
+            <SubjectDesc subdesc= 'Mobile Application'/>
+            <Units units='3'/>
+          </View> 
+      </View>
+      <View style={styles.sublist}>
+        <Subject  subcode= 'IT 311'/>
+          <View>
+            <SubjectDesc subdesc= 'Software Engineering'/>
+            <Units units='3'/>
+          </View> 
+      </View>
+      <View style={styles.sublist}>
+        <Subject  subcode= 'IT 312'/>
+          <View>
+            <SubjectDesc subdesc= 'Web Development/Web Enterprise'/>
+            <Units units='3'/>
+          </View> 
+      </View>
+      <View style={styles.sublist}>
+        <Subject  subcode= 'IT 313'/>
+          <View>
+            <SubjectDesc subdesc= 'Quantitative Methods'/>
+            <Units units='3'/>
+          </View> 
+      </View>
+      <Text style={styles.clickhere}>Click here to see more</Text> 
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  head: {
+    backgroundColor: '#24469C',
+    alignItems: 'left',
+    justifyContent: 'center',
+    height: 260,
+    width: 350,
+    borderTopRightRadius: 40,
+    borderBottomRightRadius: 40,
+    marginTop:'40px',
+    marginBottom:'30px',
+    
+  },
+  pp: {
+    marginLeft: '20px',
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  },
+  profile:{
+    color: '#fff',
+    fontSize: '30px',
+    marginLeft: '20px' 
+  },
+  course: {
+    color: '#fff',
+    marginLeft: '20px',
+  },
+
+  subtitle:{
+    marginRight:'248px',
+    fontSize: '20px',
+    marginBottom:'5px'
+  },
+  sublist: {
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    alignItems:'center',
+    width: 350,
+    backgroundColor: '#DCDCDC',
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    marginBottom:'8px',
+    marginTop:'8px',
+  },
+
+  sub:{
+    fontSize: '22px', 
+    padding:5,
+    fontWeight:'bold',
+    marginLeft:'20px'
+  },
+  subdesc:{
+    marginLeft:'30px'
+  },
+  units:{
+    marginLeft:'30px'
+  },
+  clickhere:{
+    marginTop:'15px',
+    marginLeft:'180px',
+    fontSize: '16px',
+    marginBottom:'40px',
+    color:'#24469C'
+  }
 });
